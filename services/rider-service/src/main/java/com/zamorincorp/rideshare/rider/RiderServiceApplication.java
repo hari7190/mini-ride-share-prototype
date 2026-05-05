f878eeca98360e536e0480428711ebf03be13751
Hari Muraleedharan <hari7190@gmail.com>
2026-05-04 22:51:48 -0400

    A dummy change for testing build trigger.

--- services/rider-service/src/main/java/com/zamorincorp/rideshare/rider/RiderServiceApplication.java
+++ services/rider-service/src/main/java/com/zamorincorp/rideshare/rider/RiderServiceApplication.java
@@ -3,7 +3,7 @@ package com.zamorincorp.rideshare.rider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// entry point for the Rider Service application
 @SpringBootApplication
 public class RiderServiceApplication {
     public static void main (String[] args){
