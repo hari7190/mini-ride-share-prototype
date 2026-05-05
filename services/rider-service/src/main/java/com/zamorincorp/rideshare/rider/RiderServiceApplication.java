64ea0ab175c3c96f1c5263e8bfeef7bc335b9b1d
Hari Muraleedharan <hari7190@gmail.com>
2026-05-04 23:02:29 -0400

    Reverting dummy commit

--- services/rider-service/src/main/java/com/zamorincorp/rideshare/rider/RiderServiceApplication.java
+++ services/rider-service/src/main/java/com/zamorincorp/rideshare/rider/RiderServiceApplication.java
@@ -3,7 +3,6 @@ package com.zamorincorp.rideshare.rider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// entry point for the Rider Service application
 @SpringBootApplication
 public class RiderServiceApplication {
     public static void main (String[] args){
