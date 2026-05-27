8c398740551d127f1c6451adbe726e051b549afa
Hari Muraleedharan <hari7190@gmail.com>
2026-05-26 20:33:04 -0400

    Creating payment-service stub with grpc for payment method verification

---
+++ services/payment-service/payment-service/src/test/java/com/zamorincorp/rideshare/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamorincorp.rideshare.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
